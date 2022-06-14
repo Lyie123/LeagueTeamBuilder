e42be2f72273d617a2226acb7900d0d13af5f585
Lyie123 <moch.rene@googlemail.com>
2022-06-14 21:13:55 +0200

    Update league_champion_win.sql

--- data/league_champion_win.sql
+++ data/league_champion_win.sql
@@ -1,9 +1,13 @@
-select game_id, team_id, participant_id, champion_name, win
+with games as
+(
+    select distinct game_id
+    from participants
+    where
+        summoner_name in(
+            select summoner_name
+            from league_item
+            where league_id =  '04a0f903-37c7-316f-af90-5f8eed42011c'
+        )
+)
+select game_id, team_id, team_position, champion_name, win
 from participants
-where
-    summoner_name in(
-        select summoner_name
-        from league_item
-        where
-            league_id = '04a0f903-37c7-316f-af90-5f8eed42011c'
-    );
